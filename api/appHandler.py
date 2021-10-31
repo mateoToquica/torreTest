a674c1f4d937d45239f74dab730ac7cc776970b4
Mateo Toquica Arango <mtoquica@quantum-x.io>
2021-10-31 17:54:22 -0500

    Adding front end work

--- api/appHandler.py
+++ api/appHandler.py
@@ -1,21 +1,27 @@
 import json
-from flask import Flask
+from flask import Flask, request
 import requests
 import sys
+
 sys.path.insert(0, '/controllers/validator_handler/validator_handler.py')
 from controllers.validator_handler.validator_handler import validator_quantity
+from flask_cors import CORS
+
 
 
 
 app = Flask(__name__)
-
+CORS(app, resources={r"/*": {"origins": "*"}})
 @app.route("/candidate", methods=["POST"])
-def app_handler(event):
-    name = event.get('name')
-    years = event.get('years')
-    titles = event.get('titles')
+def app_handler():
+    name = request.json['name']
+    years = request.json['work_experience']
+    titles = request.json['titles']
     req = f"https://torre.bio/api/bios/{name}".format(name = name)
     r = requests.get(req)
     json_value = r.json()
     tmp = validator_quantity(json_value, years, titles)
     return tmp
+
+if __name__ == "__main__":
+    app.run()
--- api/controllers/validator_handler/validator_handler.py
+++ api/controllers/validator_handler/validator_handler.py
@@ -1,5 +1,7 @@
 import datetime
 
+from requests.api import get
+
 def validator_quantity(json_value, years, titles):
     tmp = json_value.get('education')
     validator = 'No aplica'
@@ -7,23 +9,33 @@ def validator_quantity(json_value, years, titles):
         validator =  'Aplica'
     if count_years(json_value) > int(years)*12:
         validator = 'Aplica'
+    print(validator)
     return validator
-
+def get_now():
+    return datetime.datetime.now()
 def count_years(json_value):
     months = {'January': 1, 'February': 2, 'March': 3, 'April':4, 'May':5, 'June':6, 'July':7, 'August':8, 'September':9, 'October':10, 'November':11, 'December':12}
     experiences = json_value.get('experiences')
     cont_months = 0
     for experience in experiences:
         if experience.get('category') == 'jobs':
+            
+        
             from_month = (months[experience.get('fromMonth')])
-            from_year = experience.get('fromMonth')
-
-            to_month = (months[experience.get('fromMonth')])
-            to_year = experience.get('fromMonth')
+            from_year = experience.get('fromYear')
+            if experience.get('fromMonth') != None and experience.get('toYear') != None:
+                to_month = (months[experience.get('toMonth')])
+                to_year = experience.get('toYear')
+            else:
+                to_month = None
+                to_year = None
 
             start_date = datetime.datetime(int(from_year), from_month, 1)
-            end_date = datetime.datetime(int(to_year), to_month, 1)
+            if to_month != None and to_year != None:
 
+                end_date = datetime.datetime(int(to_year), to_month, 1)
+            else:
+                end_date = get_now()
             num_months = (end_date.year - start_date.year) * 12 + (end_date.month - start_date.month)
             cont_months += num_months
     return cont_months
